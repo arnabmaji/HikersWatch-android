5a8cb49f3907f9bced41ba5b0629180842ed0de1
arnabmaji19 <arnabmaji1999@gmail.com>
2019-08-17 14:12:36 +0530

    Done!

--- app/src/main/java/com/arnab/hikerswatch/MainActivity.java
+++ app/src/main/java/com/arnab/hikerswatch/MainActivity.java
@@ -1,14 +1,114 @@
 package com.arnab.hikerswatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView latitudeTextView;
+    TextView longitudeTextView;
+    TextView accuracyTextView;
+    TextView altitudeTextView;
+    TextView addressTextView;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                   locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+               }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latitudeTextView = findViewById(R.id.latitudeTextView);
+        longitudeTextView = findViewById(R.id.longitudeTextView);
+        accuracyTextView = findViewById(R.id.accuracyTextView);
+        altitudeTextView = findViewById(R.id.altitudeTextView);
+        addressTextView = findViewById(R.id.addressTextView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                updateInfo(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        } else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        }
+    }
+
+    private void updateInfo(Location currentLocation){
+        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+
+        try{
+            List<Address> addressList = geocoder.getFromLocation(currentLocation.getLatitude(),currentLocation.getLongitude(),1);
+            if(addressList != null && addressList.size()>0){
+                Address currentAddress = addressList.get(0);
+                String latitude = "" + currentLocation.getLatitude();
+                String longitude = "" + currentLocation.getLongitude();
+                String accuracy = "" + currentLocation.getAccuracy();
+                String altitude = "" + currentLocation.getAltitude();
+                String address = currentAddress.getLocality() + " " + currentAddress.getPostalCode() +
+                        "\n" + currentAddress.getAdminArea();
+
+                latitudeTextView.setText(latitude);
+                longitudeTextView.setText(longitude);
+                accuracyTextView.setText(accuracy);
+                altitudeTextView.setText(altitude);
+                addressTextView.setText(address);
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Error Occurred!",Toast.LENGTH_SHORT).show();
+        }
     }
 }
